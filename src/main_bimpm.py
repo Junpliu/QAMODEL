bfcefb374434e5bc3477267bf6af66ee05f019af
aitingliu <aitingliu@tencent.com>
2019-07-22 21:15:20 +0800

    fix main_bimpm.py for venus

--- src/main_bimpm.py
+++ src/main_bimpm.py
@@ -29,15 +29,15 @@ flags = tf.flags
 FLAGS = flags.FLAGS
 
 # save model path
-flags.DEFINE_string("model_dir", "model/qq_simscore/BiMPM", "model path")
+flags.DEFINE_string("model_dir", "/ceph/qbkg/aitingliu/qq/src/model/qq_simscore/BiMPM", "model path")
 
 # data file path
-flags.DEFINE_string("train_file", "data/qq_simscore/train.txt", "Training data file.")
-flags.DEFINE_string("dev_file", "data/qq_simscore/dev.txt", "Development data file.")
-flags.DEFINE_string("test_file", "data/qq_simscore/test.txt", "Test data file.")
-flags.DEFINE_string("infer_file", "data/qq_simscore/test.txt", "Test data file.")
-flags.DEFINE_string("word_vocab_file", "data/qq_simscore/word.txt", "Word vocabulary file.")
-flags.DEFINE_string("char_vocab_file", "data/qq_simscore/char.txt", "Char vocabulary file.")
+flags.DEFINE_string("train_file", "/ceph/qbkg/aitingliu/qq/src/data/qq_simscore/train.txt", "Training data file.")
+flags.DEFINE_string("dev_file", "/ceph/qbkg/aitingliu/qq/src/data/qq_simscore/dev.txt", "Development data file.")
+flags.DEFINE_string("test_file", "/ceph/qbkg/aitingliu/qq/src/data/qq_simscore/test.txt", "Test data file.")
+flags.DEFINE_string("infer_file", "/ceph/qbkg/aitingliu/qq/src/data/qq_simscore/test.txt", "Test data file.")
+flags.DEFINE_string("word_vocab_file", "/ceph/qbkg/aitingliu/qq/src/data/qq_simscore/word.txt", "Word vocabulary file.")
+flags.DEFINE_string("char_vocab_file", "/ceph/qbkg/aitingliu/qq/src/data/qq_simscore/char.txt", "Char vocabulary file.")
 flags.DEFINE_string("word_embed_file", None, "Pretrained embedding file.")
 flags.DEFINE_string("ckpt_name", "BiMPM.ckpt", "Checkpoint file name.")
 
